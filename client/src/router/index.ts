6232dc7e789caa480797e2abbd6f4bfbdfe92f51
AnteAntonini <anteantonini.mtk@hotmail.com>
2022-12-04 23:42:26 +0100

    add routes

---
+++ client/src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/AddressBookView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "addressBook",
+      component: HomeView,
+    },
+    {
+      path: "/form",
+      name: "form",
+      component: () => import("../views/AddressBookForm.vue"),
+    },
+  ],
+});
+
+export default router;
