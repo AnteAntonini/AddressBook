f759984e383e5265d315f8f57ee16dd3fac42133
AnteAntonini <anteantonini.mtk@hotmail.com>
2022-12-04 23:47:43 +0100

    setup project and remove unnecessary code

---
+++ client/src/components/SimpleButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { computed, toRefs } from "vue";
+
+export interface ButtonProps {
+  backgroundColor?: string;
+  textColor?: string;
+}
+const props = withDefaults(defineProps<ButtonProps>(), {
+  backgroundColor: "",
+  textColor: "",
+});
+const emit = defineEmits<{
+  (event: "click"): void;
+}>();
+const { backgroundColor, textColor } = toRefs(props);
+
+const styles = computed(() => ({
+  backgroundColor: backgroundColor.value,
+  color: textColor.value,
+}));
+</script>
+
+<template>
+  <button class="sc-btn" :style="styles" @click="emit('click')"></button>
+</template>
