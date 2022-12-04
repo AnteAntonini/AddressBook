b85430b42353de83250d0f577c86d116284c5932
AnteAntonini <anteantonini.mtk@hotmail.com>
2022-12-04 23:46:53 +0100

    add address book views

---
+++ client/src/views/AddressBookForm.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="address-book-form-wrapper">
+    <div class="address-book-form">
+      <div class="address-book-form-title">
+        <h1>Add Contact</h1>
+        <RouterLink to="/">
+          <button class="btn form-btn">X</button>
+        </RouterLink>
+      </div>
+      <div class="address-book-form-content">
+        <div class="address-book-form_input-row">
+          <h3>First Name</h3>
+          <input type="text" class="address-book-form_input" required />
+        </div>
+        <div class="address-book-form_input-row">
+          <h3>Last Name</h3>
+          <input type="text" class="address-book-form_input" required />
+        </div>
+        <div class="address-book-form_input-row">
+          <h3>Email</h3>
+          <input type="text" class="address-book-form_input" required />
+        </div>
+        <div class="address-book-form_input-row">
+          <h3>Country</h3>
+          <select
+            name="country"
+            id="country"
+            class="address-book-form_input"
+            required
+          >
+            <option>Select country</option>
+            <option></option>
+          </select>
+        </div>
+      </div>
+      <div class="address-book-form-action">
+        <button>Create</button>
+        <button>Cancel</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.address-book-form {
+  height: 60%;
+  width: 40%;
+  background-color: #fff;
+
+  &-title {
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 2rem;
+  }
+
+  &_input {
+    width: 70%;
+    padding: 0.75rem;
+
+    &-row {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 1rem 2rem;
+    }
+  }
+
+  &-action {
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+  }
+
+  &-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    background-color: var(--vt-c-white-mute);
+  }
+}
+
+.form-btn {
+  color: grey;
+  padding: 0 0;
+  font-size: 1.5rem;
+  background-color: transparent;
+}
+</style>
---
+++ client/src/views/AddressBookView.vue
@@ -0,0 +1,126 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import {
+  type AddressBookTableItem,
+  useAddressBookStore,
+} from "@/stores/address-book";
+
+const tableHeaders = [
+  {
+    value: "firstName",
+    text: "First Name",
+  },
+  {
+    value: "lastName",
+    text: "Last Name",
+  },
+  {
+    value: "email",
+    text: "Email",
+  },
+  {
+    value: "country",
+    text: "Country",
+  },
+];
+
+const tableItems = ref<Array<AddressBookTableItem>>([]);
+tableItems.value = useAddressBookStore().addressBookItems;
+</script>
+
+<template>
+  <main class="address-book">
+    <h1 class="address-book-title">Address Book</h1>
+    <RouterLink to="/form">
+      <button class="btn btn-large btn-primary">Add Contact</button>
+    </RouterLink>
+    <div class="address-book-table-wrapper">
+      <table class="address-book-table">
+        <thead>
+          <tr>
+            <th v-for="{ text, value } in tableHeaders" :key="value">
+              {{ text }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <template v-for="item in tableItems" :key="item.email">
+            <tr class="address-book-table-row">
+              <td>{{ item.firstName }}</td>
+              <td>{{ item.lastName }}</td>
+              <td>{{ item.email }}</td>
+              <td>{{ item.country }}</td>
+              <RouterLink to="/form">
+                <button class="btn btn-primary btn-spacing">Edit</button>
+              </RouterLink>
+              <button class="btn btn-warning">Delete</button>
+            </tr>
+          </template>
+        </tbody>
+      </table>
+    </div>
+  </main>
+</template>
+
+<style scoped lang="scss">
+.address-book {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 4rem 10rem;
+
+  &-title {
+    padding-bottom: 2rem;
+  }
+
+  &-table-wrapper {
+    overflow-x: auto;
+    overflow-y: auto;
+    width: 100%;
+    height: 100%;
+    background-color: var(--color-background);
+  }
+
+  &-table {
+    --table-padding-x: 1.5rem;
+    width: 100%;
+    color: var(--sc-text-primary-1);
+    border-spacing: 0;
+    border-collapse: collapse;
+    overflow-x: auto;
+
+    &-row {
+      border-bottom: 1px solid lightgray;
+    }
+
+    thead {
+      background-color: var(--sc-bg-table-header);
+      text-align: left;
+      tr {
+        &:hover {
+          background-color: var(--sc-bg-table-header);
+        }
+      }
+      tr th {
+        padding: 1.5rem var(--table-padding-x) 0.625rem;
+        color: var(--sc-text-primary-2);
+        font-size: 0.75rem;
+        text-transform: uppercase;
+        word-break: break-word;
+      }
+    }
+
+    tr td {
+      padding-left: var(--table-padding-x);
+      padding-right: var(--table-padding-x);
+      word-break: break-word;
+      height: 3rem;
+    }
+  }
+}
+
+.btn-spacing {
+  margin: 0.5rem 1rem;
+}
+</style>
