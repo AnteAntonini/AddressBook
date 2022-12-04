990c1a3e5da879766d1f370de97a9fbea5f99243
AnteAntonini <anteantonini.mtk@hotmail.com>
2022-12-04 23:41:26 +0100

    add address book test

---
+++ client/src/components/__tests__/AddressBook.spec.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect, beforeEach } from "vitest";
+import { setActivePinia, createPinia } from "pinia";
+
+import { mount } from "@vue/test-utils";
+import AddressBookView from "@/views/AddressBookView.vue";
+
+describe("AddressBookView", () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  it("renders properly", () => {
+    const wrapper = mount(AddressBookView);
+    expect(wrapper.text()).toContain("Address Book");
+  });
+});
